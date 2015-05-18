ef251d59f5e0d391221a583f844d81f1e08bcd76
Dan McGough <mcgough.dan@gmail.com>
2015-05-18 09:55:07 -0700

    creddle resume link added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 $(function(){
-  $('.header').hide();
-  $('.header').fadeIn(3000);
-  // $('img').foggy({
-  //  blurRadius: 5,          // In pixels.
-  //  opacity: 0.5,           // Falls back to a filter for IE.
-  //  cssFilterSupport: true  // Use "-webkit-filter" where available.
-  // });
-  // $('img').each(function(){
-  //   $(this).on('mouseover',function(){
-  //     $(this).foggy(false);
-  //   $(this).on('mouseout',function(){
-  //     $(this).foggy(true)
-  //   })
-  //   })
-  // })
+
 
 })
