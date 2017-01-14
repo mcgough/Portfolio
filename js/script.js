a5c3014e035863ed97679c61e2d1f319e086cd09
Dan <mcgough.dan@gmail.com>
2017-01-14 00:28:35 -0800

    animation added to intro and contact

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
-$(function(){
-  
-  var $scroll;
+function scrollEffect (dist) {
+	return $("html, body").animate({ 
+			scrollTop: dist 
+		});
+}
 
-	$(window).scroll(function() {
-  	$scroll = document.body.scrollTop;
+function fadeIn () {
+	$('.intro-message h1').addClass(opaque);
+	$('.intro-divider').addClass(opaque);
+	$('.intro-social-buttons').addClass(opaque);
+	$('.intro-message h3').addClass(opaque);
+}
+
+function fadeOut () {
+	$('.intro-message h1').removeClass(opaque);
+	$('.intro-divider').removeClass(opaque);
+	$('.intro-social-buttons').removeClass(opaque);
+	$('.intro-message h3').removeClass(opaque);
+}
+
+var opaque = 'opaque';
 		
+$(function(){
+
+	var $scrollTop,
+		  $footerContact = $('#contact .row'),
+		  $footerDistance;
+	
+	$(window).scroll(function() {
+		
+		$scrollTop = document.body.scrollTop;
+		$footerDistance = $footerContact.offset().top - $scrollTop;
+
+		$scrollTop > 50 ? fadeIn() : fadeOut()
+		$footerDistance > 500 ? $footerContact.addClass(opaque) : $footerContact.removeClass(opaque);
+
 	})
+
 })
+		
