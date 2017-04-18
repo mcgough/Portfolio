90eb7734d5e61b11a1530e06c09ac6d53bd79f9b
Dan McGough <mcgough.dan@gmail.com>
2017-04-18 15:59:59 -0700

    scrollmagic added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
+
+
 function scrollEffect (dist,time) {
-	return $("html, body").animate({ 
-			scrollTop: dist 
+	return $("html, body").animate({
+			scrollTop: dist
 		},time);
 }
 
 function introFade(scroll) {
+	var scale = scroll * 0.02;
 	document.querySelector('.intro-message').style.opacity = 1 - (scroll * 0.0075);
 }
 
 function footerFade(scroll) {
-	var diff = scroll * -.0075 + .5;
+	var diff = scroll * -0.0075 + 0.5;
 	document.querySelector('#contact .container .row').style.opacity = diff;
 }
 
 function closeNavbar () {
 	$('.content-link').on('click', function() {
 		$('.navbar-collapse').removeClass('in');
-		$('.navbar-toggle').removeClass('open')
-	})
+		$('.navbar-toggle').removeClass('open');
+	});
 }
 
 function openNav () {
 	$('.navbar-toggle').on('click', function() {
 		$('.navbar-toggle').toggleClass('open');
-	})
+	});
 }
 
 function chevronClick () {
 	$('.chevron').on('click', function() {
 		var dist = $('#about').offset().top;
 		scrollEffect(dist,1000);
-	})
+	});
 }
 
 function storageCheck () {
@@ -59,7 +62,7 @@ function welcome () {
 			"Back again? I'd love to hear what you think. Let's connect!",
 			"Stopped by to take another look, huh? Let's connect!"
 		]
-	}
+	};
 	if (storageCheck()) {
 		var visited = window.localStorage.getItem('visited'),
 				$copy = $('.welcome-container .copy');
@@ -75,37 +78,76 @@ function welcome () {
 }
 
 var opaque = 'opaque';
-		
+
 $(function() {
 
-	var $scrollTop,
+	var controller = new ScrollMagic.Controller(),
+			$scrollTop,
 		  $footerContact = $('#contact'),
 		  $footerDistance,
 		  $navBar = $('#myNavBar');
-	
+
+	$('.section').each(function() {
+		var id = $(this).attr('id');
+		new ScrollMagic.Scene({
+			triggerElement: '#' + id,
+			triggerHook: 0
+		})
+		.setPin('#' + id)
+		.addIndicators()
+		.addTo(controller);
+	});
+
+	// var introHeader = new ScrollMagic.Scene({
+	// 	triggerElement: '.chevron',
+	// 	triggerHook: -100
+	// })
+	// .setClassToggle('.intro-header','active')
+	// .addIndicators()
+	// .addTo(controller);
+
+
+	// var aboutMe = new ScrollMagic.Scene({
+	// 	triggerElement: '#about',
+	// 	triggerHook: 0
+	// })
+	// .setPin('#about')
+	// .addIndicators()
+	// .addTo(controller);
+
+	// var skills = new ScrollMagic.Scene({
+	// 	triggerElement: '#skills',
+	// 	offset: -100
+	// })
+	// .setClassToggle('#skills','active')
+	// .addIndicators()
+	// .addTo(controller);
+
 	$(window).scroll(function() {
 
 		$scrollTop = document.body.scrollTop;
 		$footerDistance = $footerContact.offset().top - $scrollTop;
-		
+
 		if ($scrollTop <= 300) {
 			introFade($scrollTop);
 		}
-		
+
 		footerFade($footerDistance);
 		$navBar.addClass('scrolling');
 		setTimeout(function () {
 			$navBar.removeClass('scrolling');
 		},1000);
-		
-	})
-			
+
+	});
+
+
+
 	welcome();
 	chevronClick();
 	openNav();
 	closeNavbar();
 
-})
+});
 
 
-		
+
